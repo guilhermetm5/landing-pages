280fe9b7fe5c7f0920b0454f069ed2612900f9de
João Guilherme <joaosilvabarbosa001@gmail.com>
2024-06-24 08:36:59 -0300

    script para efeito de revelar em js

--- Projetos/Clash-of-Clans/scripts/script.js
+++ Projetos/Clash-of-Clans/scripts/script.js
@@ -1,1 +1,20 @@
 // Script JavaScript para o projeto
+let menu = document.querySelector('#icon-menu');
+let navlist = document.querySelector('navlist');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navlist.classList.toggle('open');
+};
+
+const sr = ScrollReveal({
+  distance: '65px',
+  duration: 2600,
+  delay: 450,
+  reset: true,
+});
+
+sr.reveal('.witch-text', { delay: 200, origin: 'top' });
+sr.reveal('.witch-img', { delay: 450, origin: 'top' });
+sr.reveal('.icons', { delay: 500, origin: 'left' });
+sr.reveal('.scroll-down', { delay: 500, origin: 'down' });
